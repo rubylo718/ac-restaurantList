5d87e67e4c0d05c90cf161dbf01b14b6726264cb
rubylo718 <rubylo718@gmail.com>
2022-04-11 17:25:24 +0800

    feat: add search by category

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword)
   })
   res.render('index', { restaurants: restaurants, keyword: keyword })
 })
