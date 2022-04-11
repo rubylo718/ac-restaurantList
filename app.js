c16f757de56023dca7858723f04918e060b7ae1a
rubylo718 <rubylo718@gmail.com>
2022-04-11 14:47:32 +0800

    feat: import the json file of restaurant list

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 import express from 'express'
 import exphbs from 'express-handlebars'
 
+import { createRequire } from "module" // Bring in the ability to create the 'require' method
+const require = createRequire(import.meta.url) // construct the require method
+const restaurantList = require('./restaurant.json') // use the require method
+
 const app = express()
 const port = 3000
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurantList: restaurantList})
 })
 
 app.listen(port, () => {
