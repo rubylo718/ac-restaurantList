09a581d0d288d948c18f96cf0d5d1457d3b33727
rubylo718 <rubylo718@gmail.com>
2022-04-11 14:31:26 +0800

    feat: introduce express-handlebars and add pages

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from 'express'
+import exphbs from 'express-handlebars'
 
 const app = express()
 const port = 3000
 
-app.get('/', (req,res) => {
-  console.log('hello')
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
 
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
 app.listen(port, () => {
